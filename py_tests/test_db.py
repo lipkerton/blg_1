72a726c1689de511a3a6977777f2fcd09df1f624
lipkerton <petr.petuxof@yandex.ru>
2025-08-15 09:51:02 +0300

    test_db.py pylint fixes

--- py_tests/test_db.py
+++ py_tests/test_db.py
@@ -1,20 +1,34 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncEngine
+"""
+Тесты для первичной провеки коннекта к БД.
+"""
 import pytest
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine
+)
 
 from ..app.config import settings
-
 
 pytestmark = pytest.mark.db
 
 
 def test_engine():
+    """
+    Делаем движок + проверяем, что он был создан.
+    """
     engine = create_async_engine(settings.POSTGRES_URL)
     assert isinstance(engine, AsyncEngine), \
         f"Не удалось подключиться к БД с помощью запроса: {settings.POSTGRES_URL}."
 
 
 async def test_session():
+    """
+    Снова делаем движок + от него делаем сессию
+    и проверяем, что сессия была создана.
+    """
     engine = create_async_engine(settings.POSTGRES_URL)
     async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
     async with async_session_maker() as session:
@@ -23,9 +37,10 @@ async def test_session():
 
 
 async def test_query(session):
+    """
+    Делаем ORM запрос к БД на проверку работы.
+    """
     query = select(1)
     result = await session.execute(query)
     assert result.scalar() == 1, \
         "Запрос `select(1)` вернул неверный результат."
-
-
