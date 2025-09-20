ed2ab77f594e2a5eb1a24b752f667da103c99ad7
lipkerton <petr.petuxof@yandex.ru>
2025-09-20 14:37:59 +0300

    docker-compose completed

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ from app.post import post
 from app.user import user
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):  # pylint: disable=unused-argument, disable=redefined-outer-name
-#     """
-#     Инициализация БД при старте приложения.
-#     """
-#     await database.setup_db()
-#     yield
-#
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # pylint: disable=unused-argument, disable=redefined-outer-name
+    """
+    Инициализация БД при старте приложения.
+    """
+    await database.setup_db()
+    yield
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(homepage.router)
 app.include_router(post.router)
 app.include_router(user.router)
